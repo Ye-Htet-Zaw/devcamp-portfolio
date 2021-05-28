03887af2c3d1035f27d836bc46a8ef25d22ba228
YeHtet <yehtetzaw.gic@gmail.com>
2021-05-28 15:08:46 +0630

    Updated status for enum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_28_065718) do
+ActiveRecord::Schema.define(version: 2021_05_28_082527) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2021_05_28_065718) do
     t.integer "percent_utilized"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status", default: 0
   end
 
 end
