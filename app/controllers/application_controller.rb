7ad0ee903850a4151ed0366245e476e5c669f696
YeHtet <yehtetzaw.gic@gmail.com>
2021-06-01 09:18:38 +0630

    Added nev bar and added default header content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
 	include SetSource
 
 	include CurrentUserConcern
+
+	before_action :set_page_defaults
+
+	def set_page_defaults
+    @page_title = "Devcamp Portfolio | My Portfolio Website"
+    @seo_keywords = "Jordan Hudgens portfolio"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-  get 'about', to: 'pages#about'
+  get 'about_me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
   #get 'pages/about'
